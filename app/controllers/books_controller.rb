63e9d0c2ff9f821b6fb5fb6bb2c6ff16e7057acc
Nuri1977 <nurilacka@gmail.com>
2021-09-26 20:10:24 +0200

    Exception handling in controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def index
     render json: Book.all
   end
@@ -24,5 +25,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :author)
   end
-  
+
 end
